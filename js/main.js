1c8f27d18bca29f4bec326594622f078c4b37f77
VIRUS <levinhtxbt@gmaill.com>
2017-08-13 19:15:55 +0700

    update-sample-2

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
 import expect from 'expect'
 var deepFreeze = require('deep-freeze');
+
+const incrementCounter = (list, index) => {
+    //impure function 
+    // list[index]++; 
+    // return list;
+
+    //Avoiding array mutations with sclice(), concat()
+    // return list
+    //     .slice(0, index)
+    //     .concat(list[index] + 1)
+    //     .concat(list.slice(index + 1));
+    
+    //Avoiding array mutations with spread syntax
+    return [
+        ...list.slice(0, index),
+        list[index] + 1,
+        ...list.slice(index + 1)
+    ];
+}
+
+const testIncrementCounter = () => {
+    const listBefore = [10, 20, 30];
+    const listAfter = [10, 21, 30];
+
+    //deepFree
+    deepFreeze(listBefore);
+
+    expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+}
+
+testIncrementCounter();
+
+console.log("Test passed");
