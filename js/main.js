285dab85670f060dc0b22b4408f6e59054badd4d
VIRUS <levinhtxbt@gmaill.com>
2017-08-13 18:52:18 +0700

    addCounter

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 import expect from 'expect'
 var deepFreeze = require('deep-freeze');
+
+const addCounter = (list, number) => {
+    //Impure function
+    // list.push(number);
+    // return list;
+
+    return list.concat(number);
+}
+
+const testAddCounter = () => {
+    const listBefore = [];
+    const listAfter = [10];
+    
+    //add deepFreeze here
+    deepFreeze(listBefore);
+
+    expect(
+        addCounter(listBefore, 10)
+    ).toEqual(listAfter);
+}
+
+testAddCounter();
+
+console.log("Test passed!!!");
+
